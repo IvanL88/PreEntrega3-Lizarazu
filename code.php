328ce1f524a73f8f4170cc066a32c0e704f9efbe
IvanL88 <40809110+IvanL88@users.noreply.github.com>
2022-11-25 09:47:32 -0300

    Update code.php

--- code.php
+++ code.php
@@ -2,46 +2,45 @@
 
 
 
-  public function enviarMailPorAuth($objMail=null)
-    //Envio de mail por usuario, pero con auth de google
-    { 
-        //ENVIO DE MAILS
-        $mail = new LATCOMSender();
-
-        $mail->AuthType     = "XOAUTH2"; //Tipo de autenticación SMTP.
-        $mail->CharSet      = "UTF-8"; //El conjunto de caracteres del mensaje.       
-        $mail->From         = $objMail->fromMail; //La dirección de correo electrónico del remitente del mensaje.
-        $mail->FromName     = $objMail->fromName; //El nombre de del mensaje.
-        $mail->TokenSender  = $objMail->fromSenderToken; // Token Sender
-        $mail->Username     = $objMail->fromMail; //Nombre de usuario
-        $mail->Subject      = $titulo = $objMail->motivo; //Asunto 
-        
-        $content = $objMail->texto;         
-        
-        $mail->AddAddress($objMail->mail); //Al que se le envia /El Contacto
-
-        //Para las copias a enviar
-        $arr_mails=explode(';', $objMail->mails);
-        for ($i=0; $i < count($arr_mails) ; $i++) {
-            $mail->AddCC($arr_mails[$i]);
+  public function enviarPorAuth($debugEmail = NULL) {        
+        $this->isSMTP();
+        $this->isHTML(TRUE);
+        if ($debugEmail) {
+            $this->clearAddresses();
+            $this->clearAllRecipients();
+            $this->clearBCCs();
+            $this->clearCCs();
+            $this->addAddress($debugEmail);
+            $this->From = $debugEmail;
+            $this->addReplyTo($debugEmail);
+            $this->SMTPDebug = 3;
+            $this->send(TRUE);
+            echo '<pre>';
+            print_r($this);
+            exit;
+            return $this->send(TRUE);
         }
 
-        foreach ($objMail->files as $file) {
-            $mail->AddAttachment($file['path'], $file['name']);
-        }        
-       
-        $mail->AddReplyTo($objMail->fromMail); //RespoderA    
-        $mail->Body    =  utf8_decode($content); //Cuerpo
-      
-        try{
-           return $mail->enviarPorAuth();       
+        try{               
+            $provider = new Google([
+                "clientId" => $this->clientId,
+                "clientSecret" => $this->clientSecret,
+            ]);      
+            $this->setOAuth(
+                new PHPMailer\PHPMailer\OAuth([
+                    "provider"      => $provider,
+                    "clientId"      => $this->clientId,
+                    "clientSecret"  => $this->clientSecret,
+                    "refreshToken"  => $this->TokenSender,
+                    "userName"      => $this->Username,
+                ])
+            );    
+            return $this->send(TRUE);
         }catch (Exception $e) {
-            echo "Excepción capturada: ", $e->getMessage(), "\n";
-            log_message('error', 'error'); 
+            log_message('error', $e->getMessage()); 
             return false;
-        }       
-
-    } 
+        }
+    }   
 
 
 
